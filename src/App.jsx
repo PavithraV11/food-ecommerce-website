02236c90057cf6cb57b801ffd179484201e1476b
Pavithra <yourgithubemail@gmail.com>
2026-08-16 21:05:54 +0530

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
@@ -7,13 +7,14 @@ import CartDrawer from "./components/CartDrawer";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/food-ecommerce-website">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Product />} />
         <Route path="/reviews" element={<Reviews />} />
       </Routes>
+
       <CartDrawer />
     </BrowserRouter>
   );
